56188bbf8953075d28a33d59eb5422ecced9494f
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-08 16:26:14 -0300

    Add MovieDetail ui model

---
+++ AppPatronesLuisHerreraLillo/UI/Detail/Models/MovieDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetail.swift
+//  AppPatronesLuisHerreraLillo
+//
+//  Created by Luis Eduardo Herrera Lillo on 08-10-23.
+//
+
+import Foundation
+
+struct MovieDetail {
+    let title: String
+    let genre: String?
+    let overview: String
+    let posterUrl: String?
+    let backdropUrl: String?
+    let releaseDate: String
+    let voteAverage: Double
+}
