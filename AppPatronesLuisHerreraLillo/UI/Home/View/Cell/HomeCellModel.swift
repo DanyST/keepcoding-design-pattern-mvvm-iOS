5aeec93016fcb6a9fd005079cf5cae3c481cfeec
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-08 03:28:35 -0300

    Add HomeCellModel

---
+++ AppPatronesLuisHerreraLillo/UI/Home/View/Cell/HomeCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCellModel.swift
+//  AppPatronesLuisHerreraLillo
+//
+//  Created by Luis Eduardo Herrera Lillo on 08-10-23.
+//
+
+import Foundation
+
+struct HomeCellModel {
+    let title: String?
+    let genre: String?
+    let image: String?
+}
