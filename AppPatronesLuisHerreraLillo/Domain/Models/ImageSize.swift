f1b8956ae8e42a0e2bc95a8ff9240b9e505f16b2
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-08 04:52:47 -0300

    Add ImageSize Model

---
+++ AppPatronesLuisHerreraLillo/Domain/Models/ImageSize.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSize.swift
+//  AppPatronesLuisHerreraLillo
+//
+//  Created by Luis Eduardo Herrera Lillo on 08-10-23.
+//
+
+import Foundation
+
+enum ImageSize: String {
+    case w200
+    case w300
+    case w600
+}
