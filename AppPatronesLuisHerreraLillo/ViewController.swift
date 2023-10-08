58a908972078e1df7cc122f1344ae026ca49aa49
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-07 23:57:42 -0300

    Create project and add gitignore

---
+++ AppPatronesLuisHerreraLillo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppPatronesLuisHerreraLillo
+//
+//  Created by Luis Eduardo Herrera Lillo on 07-10-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
