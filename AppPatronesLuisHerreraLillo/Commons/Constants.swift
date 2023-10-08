ce6520616fa5152dcdf2d626611280a8817ea966
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-08 04:53:11 -0300

    Add Constants enum

---
+++ AppPatronesLuisHerreraLillo/Commons/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AppPatronesLuisHerreraLillo
+//
+//  Created by Luis Eduardo Herrera Lillo on 08-10-23.
+//
+
+import Foundation
+
+enum Constants {
+    static let imageBaseUrl = "https://image.tmdb.org/t/p/"
+}
