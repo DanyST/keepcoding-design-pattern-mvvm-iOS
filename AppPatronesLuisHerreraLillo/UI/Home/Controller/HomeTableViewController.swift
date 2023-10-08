9f71e0ec7469eda2e5dc60e3a46bc619cc302303
Luis Herrera Lillo <lherreralillo@gmail.com>
2023-10-08 03:31:07 -0300

    Refactor HomeTableViewController with HomeViewProtocol and viewModel property

--- AppPatronesLuisHerreraLillo/UI/Home/Controller/HomeTableViewController.swift
+++ AppPatronesLuisHerreraLillo/UI/Home/Controller/HomeTableViewController.swift
@@ -7,12 +7,29 @@
 
 import UIKit
 
-class HomeTableViewController: UITableViewController {
-        
+protocol HomeViewProtocol: AnyObject {
+    func updateViews()
+    func navigateToDetail(with movie: Movie)
+}
+
+final class HomeTableViewController: UITableViewController {
+    
+    private let viewModel: HomeViewModelProtocol
+    
+    init(viewModel: HomeViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Discover Movies"
+        title = viewModel.title
         registerCells()
+        viewModel.onViewsLoaded()
     }
     
     private func registerCells() {
@@ -28,14 +45,33 @@ class HomeTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModel.dataCount
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.reuseIdentifier, for: indexPath) as? HomeTableViewCell else {
             return UITableViewCell()
         }
-        cell.updateViews()
+        
+        if let data = viewModel.data(at: indexPath.row) {
+            cell.updateViews(model: HomeCellModel(title: data.title, genre: data.genreTypes?.first??.title, image: data.posterPath))
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        viewModel.onItemSelected(at: indexPath.row)
+    }
+}
+
+extension HomeTableViewController: HomeViewProtocol {
+    func updateViews() {
+        tableView.reloadData()
+    }
+    
+    func navigateToDetail(with movie: Movie) {
+        print("navigateToDetail: \(movie.title)")
     }
 }
